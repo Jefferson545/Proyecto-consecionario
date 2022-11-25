edade503c33c0b0737380eec2b9f4de99e4d9cc6
Jefferson Javier Avelar Arriza <99444943+Jefferson545@users.noreply.github.com>
2022-11-25 15:19:26 -0600

    Add files via upload

---
+++ Proyecto-Programacion-Concesionario-main/src/proyecto_concesionario/comparar.java
@@ -0,0 +1,17 @@
+ package proyecto_concesionario;
+
+/**
+ *
+ * @author Creado por César Eduardo Silva Colocho
+ */
+//Interface 
+public interface comparar {
+  public void igualQue();
+  public void arreglo();
+  
+  public void menorQue();
+  
+  public void mayorQue();
+  
+} 
+
